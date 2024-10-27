49e741914e05067d60c6add1f73f632647dcc39b
vinaybachani <blikevinay@gmail.com>
2024-10-27 16:38:18 +0530

    webhooks added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,3 +1,48 @@
+import { Webhook } from 'svix'
+import userModel from '../models/userModal.js';
 // api controller function to manage clerk users
-const clerkWebhooks = async (req, res) => {
+export const clerkWebhooks = async (req, res) => {
+    try {
+        // create a svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+        const { data, type } = req.body;
+
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+                await userModel.create(userData);
+                res.json({});
+                break;
+            }
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                res.json({});
+                break;
+            }
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                res.json({});
+            }
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.json({ success: false, message: error.message })
+    }
 }
